6aaeaaba8ad41ae03ce1671e86487e2df23fa688
Opilane TTHK <opilane@tthk.ee>
2020-11-12 13:22:53 +0200

    first commit

---
+++ DirectoriesandFiles/DirectoriesandFiles/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoriesandFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\opilane\samples";
+
+            string[] allDirectories = Directory.GetDirectories(rootPath);
+            ReadArray(allDirectories);
+
+            string[] includeNestedDirectories = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+            ReadArray(includeNestedDirectories);
+
+            string[] files = Directory.GetFiles(rootPath, "*.*", SearchOption.AllDirectories);
+            ReadArray(files);
+
+        }
+
+        public static void ReadArray(string[] array)
+        {
+            foreach(string line in array)
+            {
+                Console.WriteLine(line);
+
+
+
+            }
+
+
+        }
+
+    }
+}
---
+++ DirectoriesandFiles/WriteFilesAnDirectories/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace WriteFilesAnDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteFilePath();
+        }
+
+        public static void WriteFilePath()
+        {
+            string filePath = @"C:\Users\opilane\samples\Files.txt";
+            string directorypath = @"C:\Users\opilane\samples";
+
+            string[] files = Directory.GetFiles(directorypath, "*.*", SearchOption.AllDirectories);
+
+            File.WriteAllLines(filePath, files);
+
+        }
+
+
+
+    }
+}
